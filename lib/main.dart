4dad8978d2b2973a64a0adfa4204113deee951fe
Kishore <kishoremuthuselvan@gmail.com>
2021-07-09 19:38:21 +0530

    new chages for rcm specifics

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RCM Specifics',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: WebView(
+            javascriptMode: JavascriptMode.unrestricted,
+            initialUrl: 'https://rcm-specs.herokuapp.com/login/',
+          ),
+        ),
+      ),
+    );
+  }
+}
